faa0bc86366505f694299e688e07834cc1f48e07
Jan Schmitt <git@smittie.de>
2019-05-18 10:37:37 +0200

    .gitignore working now

---
+++ SecureTextEditor/Program.cs
@@ -0,0 +1,36 @@
+﻿using Medja;
+using Medja.OpenTk;
+using Medja.OpenTk.Themes.DarkBlue;
+using Medja.OpenTk.Rendering;
+
+namespace SecureTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var library = new MedjaOpenTkLibrary(new DarkBlueTheme());
+            library.RendererFactory = CreateRenderer;
+
+            var controlFactory = library.ControlFactory;
+            var application = MedjaApplication.Create(library);
+
+            var window = application.CreateWindow();
+            window.CenterOnScreen(800, 600);
+
+            var test = new SecureTextEditorView(controlFactory);
+            window.Content = test.dockPanel;//new SecureTextEditorView(controlFactory);
+            window.Title = "NotYetSecureTextEditor";
+
+            application.MainWindow = window;
+            application.Run();
+        }
+
+        private static IRenderer CreateRenderer()
+        {
+            var openTkRenderer = new OpenTkRenderer();
+            return openTkRenderer;
+        }
+
+    }
+}
