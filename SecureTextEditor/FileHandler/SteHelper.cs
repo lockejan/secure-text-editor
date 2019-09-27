85b588c2f01985c6a1b8496b6c74913904e09303
Jan Schmitt <git@smittie.de>
2019-09-27 16:21:13 +0200

    removed dictHelper function. added XML doc.

--- SecureTextEditor/FileHandler/SteHelper.cs
+++ SecureTextEditor/FileHandler/SteHelper.cs
@@ -1,27 +1,15 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
 
 namespace SecureTextEditor.FileHandler
 {
+    /// <summary>
+    /// Static class holding helper functions needed by file handlers or views. 
+    /// </summary>
     public static class SteHelper
     {
         /// <summary>
-        /// Helper function that returns null if key is not present in dict.
-        /// Prevents exceptions when instantiating json model.
-        /// </summary>
-        /// <param name="dictionary">result object coming from cipher engine</param>
-        /// <param name="key">key which will looked after</param>
-        /// <typeparam name="TKey">KeyType</typeparam>
-        /// <typeparam name="TValue">ValueType</typeparam>
-        /// <returns></returns>
-        public static TValue GetValueOrDefault<TKey,TValue>
-            (this IDictionary<TKey, TValue> dictionary, TKey key) =>
-            dictionary.TryGetValue(key, out var ret) ? ret : default;
-        
-        /// <summary>
-        /// Interface which can be used by other classes to get back the full path of the working DIR.
+        /// Function that returns the full path of the project root DIR.
         /// </summary>
         public static String WorkingDirectory
         {
