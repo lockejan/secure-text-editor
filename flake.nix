28e8d56a53ee5542936822ccccb9e10d9e64756e
Jan Schmitt <git@smittie.de>
2025-06-08 21:11:21 +0200

    feat: add basic flake for deps

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            bashInteractive
+            # mono
+            # msbuild
+            dotnet-sdk
+            dotnetPackages.Nuget
+            doxygen
+            texlivePackages.epstopdf # needed for doxygen
+          ];
+          buildInputs = [ ];
+        };
+      });
+}
